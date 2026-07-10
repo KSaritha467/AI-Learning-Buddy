f5798cd8fda53952ee16753c620dff1eeeeb702b
K Saritha <ksaritha467@gmail.com>
2026-07-10 19:21:24 +0530

    Refactor Gemini API configuration method
    Updated Gemini API configuration to use Streamlit Secrets instead of environment variables.

--- app.py
+++ app.py
@@ -1,12 +1,8 @@
 
 import streamlit as st
 import google.generativeai as genai
-import os
-
-# Configure Gemini API
-# Fetch the API key from environment variables
-genai.configure(api_key=os.environ.get('GOOGLE_API_KEY'))
-
+# Configure Gemini API using Streamlit Secrets
+genai.configure(api_key=st.secrets["GOOGLE_API_KEY"])
 model = genai.GenerativeModel("gemini-2.5-flash")
 
 st.set_page_config(page_title="AI Learning Buddy Saritha", page_icon="🎓")
